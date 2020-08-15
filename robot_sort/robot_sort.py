b4fc3559d4e88680fe1b9ed1a3f77629b0e4f549
Aaron Williams <aaronrw4@yahoo.com>
2020-08-14 20:38:01 -0700

    removed self.list

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -109,9 +109,8 @@ class SortingRobot:
                 self.move_left()
                 if self.compare_item() == None:
                     self.swap_item()
-                    return self._list
+                    return self.set_light_on
             self.swap_item()
-        return self._list
 
 
 if __name__ == "__main__":
